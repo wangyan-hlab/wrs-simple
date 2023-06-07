5116e21063df435160c25be9d06305c192fd7288
wangyan-hlab <yan@hlab.sys.es.osaka-u.ac.jp>
2023-06-07 21:34:09 +0800

    update ur5e_planning_real

--- 0000_examples/wangyan/ur5e_planning_real.py
+++ 0000_examples/wangyan/ur5e_planning_real.py
@@ -10,13 +10,14 @@ from motion.probabilistic import rrt_connect as rrtc
 from basis import robot_math as rm
 from robot_con.ur.ur5e import UR5ERtqHE as ur5e_real
 
-global path 
+global path, endtask 
 path = []
 pathLock = Lock()
+endtask = 0
 
 def run_realrobot(real_robot):
 
-    global path
+    global path, endtask
     start_jnts = [-111.817, -87.609, -118.858, -55.275, 107.847, 20.778]
     
     # Move to start_jnts
@@ -27,6 +28,8 @@ def run_realrobot(real_robot):
         robot_r.move_jnts(start_jnts)
     else:
         print("I am not a robot.")
+        time.sleep(5)
+        endtask = 1
     
     # Control real robot to reproduce the planned path
     pathLock.acquire()
@@ -45,11 +48,6 @@ if __name__ == "__main__":
     goal_jnts = [-127.146, -74.498, -85.835, -40.605, 71.584, 20.790]
 
     # WRS planning simulation
-    def genSphere(pos, radius=0.005, rgba=None):
-        if rgba is None:
-            rgba = [1, 0, 0, 1]
-        gm.gen_sphere(pos=pos, radius=radius, rgba=rgba).attach_to(base)
-
     base = wd.World(cam_pos=[2, 2, 1], lookat_pos=[0, 0, 0.5], w=960, h=720)
     gm.gen_frame().attach_to(base)
     component_name = 'arm'
@@ -61,24 +59,24 @@ if __name__ == "__main__":
     start_conf = np.deg2rad(start_jnts)
     goal_conf = np.deg2rad(goal_jnts)
     robot_s.fk(component_name, start_conf)
-    robot_s.gen_meshmodel(toggle_tcpcs=True, rgba=[1,0,0,0.5]).attach_to(base)
+    robot_start = robot_s.gen_meshmodel(toggle_tcpcs=True, rgba=[1,0,0,0.5])
+    robot_start.attach_to(base)
     robot_s.fk(component_name, goal_conf)
-    robot_s.gen_meshmodel(toggle_tcpcs=True, rgba=[0,1,0,0.5]).attach_to(base)
+    robot_goal = robot_s.gen_meshmodel(toggle_tcpcs=True, rgba=[0,1,0,0.5])
+    robot_goal.attach_to(base)
     # planner
     time_start = time.time()
     rrtc_planner = rrtc.RRTConnect(robot_s)
 
-    # pathLock.acquire()
     path = rrtc_planner.plan(component_name=component_name,
-                                start_conf=start_conf,
-                                goal_conf=goal_conf,
-                                obstacle_list=[],
-                                ext_dist=0.1,
-                                max_time=300)
+                            start_conf=start_conf,
+                            goal_conf=goal_conf,
+                            obstacle_list=[],
+                            ext_dist=0.1,
+                            max_time=300)
     time_end = time.time()
     print("Planning time = ", time_end-time_start)
     print(len(path), path)
-    # pathLock.release()
 
     def update(rbtmnp, motioncounter, robot, path, armname, task):
         if motioncounter[0] < len(path):
@@ -89,24 +87,36 @@ if __name__ == "__main__":
             robot.fk(armname, pose)
             rbtmnp[0] = robot.gen_meshmodel(toggle_tcpcs=True)
             rbtmnp[0].attach_to(base)
-            genSphere(robot.get_gl_tcp(component_name)[0], radius=0.01, rgba=[1, 1, 0, 1])
+            tcp_ball = gm.gen_sphere(pos=robot.get_gl_tcp(component_name)[0], 
+                                    radius=0.01, rgba=[1, 1, 0, 1])
+            tcp_ball.attach_to(base)
             motioncounter[0] += 1
         else:
             motioncounter[0] = 0
-        return task.again
+
+        if endtask == 1:
+            robot_start.detach()
+            robot_goal.detach()
+            rbtmnp[0].detach()
+            robot.fk(armname, path[-1])
+            robot.gen_meshmodel(toggle_tcpcs=True).attach_to(base)
+            print("Task ends")
+            return task.done
+        else:
+            return task.again
 
     rbtmnp = [None]
     motioncounter = [0]
-    taskMgr.doMethodLater(0.07, update, "update",
-                            extraArgs=[rbtmnp, motioncounter, robot_s, path, component_name], appendTask=True)
-
-    thread_rr = Thread(target=lambda: run_realrobot(True), daemon=True, name="real_robot")
+    
+    thread_rr = Thread(target=lambda: run_realrobot(False), daemon=True, name="real_robot")
     thread_rr.start()
-
-    # print("程序因线程hello_world陷入阻塞")
-    # thread_hw.join(timeout=3)
     # print("程序因线程real_robot陷入阻塞")
-    # thread_rr.join(timeout=3)
+    # thread_rr.join(timeout=1)
+
+    taskMgr.doMethodLater(0.1, update, "update",
+                        extraArgs=[rbtmnp, motioncounter, robot_s, path, component_name], 
+                        appendTask=True)
 
     base.setFrameRateMeter(True)
     base.run()
+    
