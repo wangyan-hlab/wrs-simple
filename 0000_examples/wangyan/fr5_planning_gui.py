285bebe88100ff20a6ad35f459759618ef19c185
wangyan-hlab <yan@hlab.sys.es.osaka-u.ac.jp>
2023-06-10 14:37:36 +0800

    fr5 planning gui

--- 0000_examples/wangyan/fr5_planning_gui.py
+++ 0000_examples/wangyan/fr5_planning_gui.py
@@ -1,6 +1,5 @@
 import time
 import numpy as np
-from fr_python_sdk.frmove import FRCobot
 from robot_planning_gui import FastSimWorld
 
 class FRFastSimWorld(FastSimWorld):
@@ -9,22 +8,38 @@ class FRFastSimWorld(FastSimWorld):
                  up=np.array([0, 0, 1]), fov=40, w=1920, h=1080, 
                  lens_type="perspective", toggle_debug=False, 
                  auto_cam_rotate=False, backgroundcolor=None,
-                 robot_connect=False, 
-                 robot_ip='192.168.58.2', 
-                 pc_ip='192.168.58.70'):
+                 robot_connect=False,
+                 init_conf=np.zeros(6)):
         super().__init__(cam_pos, lookat_pos, up, fov, w, h, 
                          lens_type, toggle_debug, 
                          auto_cam_rotate, backgroundcolor, 
-                         robot_connect,robot_ip, pc_ip)
+                         robot_connect, init_conf)
+        
+
+    def get_robot_jnts(self):
+        """
+            获取真实机器人关节角度
+        """
 
         if self.robot_connect:
             print("[Info] 机器人已连接")
-            self.robot_r = FRCobot()
-            self.init_conf = self.robot_r.GetJointPos(unit="rad")  # 实际机器人的初始关节角度
+            self.real_robot_conf = self.robot_r.GetJointPos(unit="rad")  # 实际机器人的关节角度
         else:
             print("[Info] 机器人未连接")
-            self.init_conf = np.zeros(6)
-        
+            self.real_robot_conf = np.zeros(6)
+
+        for i in range(6):
+            self.slider_values[i][0].setValue(np.rad2deg(self.real_robot_conf)[i])
+
+        self.robot.fk(self.component_name, np.asarray(self.real_robot_conf))
+
+        if self.robot_meshmodel is not None:
+            self.robot_meshmodel.detach()
+
+        self.robot_meshmodel = self.robot.gen_meshmodel()
+        self.robot_meshmodel.attach_to(self)
+
+    
     def robot_move(self):
         """
             FR5 robot move
@@ -37,15 +52,25 @@ class FRFastSimWorld(FastSimWorld):
 if __name__ == "__main__":
 
     from robot_sim.robots.fr5 import fr5
+    from fr_python_sdk.frmove import FRCobot as fr5_real
 
     # WRS planning simulation
     robot_connect = False
     robot_ip = '192.168.58.2'
     pc_ip = '192.168.58.70'
 
-    base = FRFastSimWorld(robot_connect=robot_connect, robot_ip=robot_ip, pc_ip=pc_ip)
+    if robot_connect:
+        print("[Info] 机器人已连接")
+        robot_r = fr5_real(robot_ip=robot_ip)
+        init_conf = robot_r.GetJointPos(unit="rad")  # 实际机器人的初始关节角度
+    else:
+        print("[Info] 机器人未连接")
+        init_conf = np.zeros(6)
     
-    robot_s = fr5.FR5_robot(enable_cc=True, peg_attached=False)
+    base = FRFastSimWorld(robot_connect=robot_connect, init_conf=init_conf)
+    if robot_connect:
+        base.robot_r = robot_r
+    robot_s = fr5.FR5_robot(enable_cc=True, peg_attached=False, zrot_to_gndbase=0)
     component = 'arm'
     base.set_robot(robot_s, component)
     
--- 0000_examples/wangyan/ur5e_planning_gui.py
+++ 0000_examples/wangyan/ur5e_planning_gui.py
@@ -15,6 +15,7 @@ class URFastSimWorld(FastSimWorld):
                          auto_cam_rotate, backgroundcolor, 
                          robot_connect, init_conf)
 
+
     def get_robot_jnts(self):
         """
             获取真实机器人关节角度
@@ -23,22 +24,21 @@ class URFastSimWorld(FastSimWorld):
         if self.robot_connect:
             print("[Info] 机器人已连接")
             self.real_robot_conf = self.robot_r.get_jnt_values()  # 实际机器人的关节角度
-
-            for i in range(6):
-                self.slider_values[i][0].setValue(np.rad2deg(self.real_robot_conf)[i])
-
-            self.robot.fk(self.component_name, np.asarray(self.real_robot_conf))
-
-            if self.robot_meshmodel is not None:
-                self.robot_meshmodel.detach()
-
-            self.robot_meshmodel = self.robot.gen_meshmodel()
-            self.robot_meshmodel.attach_to(self)
-
         else:
             print("[Info] 机器人未连接")
             self.real_robot_conf = np.zeros(6)
-   
+
+        for i in range(6):
+            self.slider_values[i][0].setValue(np.rad2deg(self.real_robot_conf)[i])
+
+        self.robot.fk(self.component_name, np.asarray(self.real_robot_conf))
+
+        if self.robot_meshmodel is not None:
+            self.robot_meshmodel.detach()
+
+        self.robot_meshmodel = self.robot.gen_meshmodel()
+        self.robot_meshmodel.attach_to(self)
+    
     
     def robot_move(self):
         """
