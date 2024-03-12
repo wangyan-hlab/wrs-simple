e7373f347d822d23d158c93f9834b731b0850f1f
WANG Yan <wangyan94maomao@gmail.com>
2024-03-12 14:02:33 +0800

    fix

--- 0000_examples/fr20_mixed_pallet.py
+++ 0000_examples/fr20_mixed_pallet.py
@@ -35,7 +35,7 @@ if __name__ == '__main__':
 
     # generate the pallet frame
     pallet_size = np.array([1, 1, 1])
-    pallet_origin_pos = np.array([1.4, -1.2, -0.8])
+    pallet_origin_pos = np.array([1.5, -0.5, -0.5])
     pallet_origin_rotmat = rm.rotmat_from_euler(0, 0, np.pi/2)
     pallet_origin_homomat = rm.homomat_from_posrot(pallet_origin_pos, pallet_origin_rotmat)
     gm.gen_frame(pallet_origin_pos, pallet_origin_rotmat).attach_to(base)
@@ -44,7 +44,8 @@ if __name__ == '__main__':
     bin_homomat = rm.homomat_from_posrot(pos=bin_pos)
     gm.gen_box(homomat=bin_homomat, rgba=[0,0,0,0.1]).attach_to(base)
     # generate the real box sequence
-    box_sequence = np.array(sequences[0])
+    selected_seq_id = -1
+    box_sequence = np.array(sequences[selected_seq_id])
     box_sequence = [np.concatenate((box[:3]*0.04, (box[3:]+box[:3]*0.5)*0.04)) for box in box_sequence]
     box_suction_sequence = [box[3:]+np.array([0,0,box[2]*.5]) for box in box_sequence]
     robot_goals = []
@@ -68,12 +69,15 @@ if __name__ == '__main__':
     robot_instance.gen_meshmodel(toggle_tcpcs=True).attach_to(base)
 
     goal_jnts = []
-    for robot_goal in robot_goals:
+    for goal_id, robot_goal in enumerate(robot_goals):
         goal_jnt = robot_instance.ik(tgt_pos=robot_goal[0], 
                                      tgt_rotmat=robot_goal[1],
                                      seed_jnt_values=start_jnt)
         if goal_jnt is not None:
             goal_jnts.append(goal_jnt)
+        else:
+            print("Infeasible goal_id:", goal_id)
+            goal_jnts.append(goal_jnts[-1])
     
     def update(box_sequence, goal_jnts, show_box_sequence, show_robot_sequence, robot, component_name, counter, color_list, task):
         if counter[0] < len(box_sequence):
@@ -86,6 +90,7 @@ if __name__ == '__main__':
                                 rm.homomat_from_posrot(pos=box[3:]))
                 rgba = color_list[counter[0] % len(color_list)]
                 show_box_sequence.append(gm.gen_box(extent=box[:3], homomat=box_homomat, rgba=rgba))
+                print("lenboxseq:", len(show_box_sequence))
                 show_box_sequence[-1].attach_to(base)
                 robot.fk(component_name, goal_jnts[counter[0]])
                 show_robot_sequence[0] = robot.gen_meshmodel(toggle_tcpcs=True, rgba=[1,1,0,0.3])
@@ -94,9 +99,15 @@ if __name__ == '__main__':
                 counter[0] += 1
                 base.inputmgr.keymap['space'] = False
         else:
-            for box in show_box_sequence:
-                box.detach()
-            counter[0] = 0
+            if base.inputmgr.keymap['space']:
+                print("reset")
+                while len(show_box_sequence) > 0:
+                    show_box_sequence.pop(0).detach()
+                if show_robot_sequence[0] is not None:
+                    show_robot_sequence[0].detach()
+                counter[0] = 0
+                base.inputmgr.keymap['space'] = False
+
         return task.cont
     
     show_box_sequence = []
